e13f026e13e88500c1ef27384fb6d2b420add954
Nicholas Joannette <nicholas.joannette@gmail.com>
2021-07-03 02:02:04 -0400

    Height based coloring added.

--- PA1_Practice/Shaders/blockShader.fs
+++ PA1_Practice/Shaders/blockShader.fs
@@ -9,7 +9,7 @@ in vec3 FragPos;
 void main()
 {   
 
-	vec3 GLOBAL_LIGHT_POSITION = vec3(50,50,50);
+	vec3 GLOBAL_LIGHT_POSITION = vec3(300,300,-300);
 	vec3 GLOBAL_LIGHT_COLOR = vec3(1.0,1.0,1.0);
 	vec3 norm = normalize(Normal);
 	vec3 lightDir = normalize(GLOBAL_LIGHT_POSITION - FragPos); 
@@ -17,7 +17,7 @@ void main()
 	float diff = max(dot(norm, lightDir), 0.0);
 	vec3 diffuse = diff * GLOBAL_LIGHT_COLOR;
 	
-	vec3 result = diffuse * vec3(0.2,0.4,0.26);
+	vec3 result = diffuse * vec3(0.4-0.015*FragPos.y,0.4+0.013*FragPos.y,0.26 + 0.01*FragPos.y);
 	FragColor = vec4(result,1.0); 
 };
 
