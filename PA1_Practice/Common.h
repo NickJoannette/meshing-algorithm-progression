03247e394c55ece63dee905b4021efb2c1b808de
Nicholas Joannette <nicholas.joannette@gmail.com>
2021-07-03 00:51:55 -0400

    More efficient meshing, dynamic sizing of chunk. Doing run-length encoding now.

--- PA1_Practice/Common.h
+++ PA1_Practice/Common.h
@@ -2,9 +2,19 @@
 
 
 
+struct Voxel {
+	vec3 start;
+	float density;
+	int type;
+	bool culled = false;
+};
+
+
+
 struct Vertex {
 	float x, y, z;
 	float nx, ny, nz;
 	float tx, ty;
 	int type;
 };
+
